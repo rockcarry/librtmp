7c09d1d4ff81a3748602c9d71b837dcaf5096d10
chenk <chenk@apical.com.cn>
2023-05-27 11:45:08 +0800

    librtmp library binary release v1.0.0.
    build by source code commit id: ced31235a5e0c4866c2ffe31f36c0f20e0a96531
    
    Change-Id: Ib3254265abac0f52aa723395e04da2ae5e6057bf

---
+++ msc3xx-glibc/rtmppush.h
@@ -0,0 +1,28 @@
+#ifndef __RTMPPUSH_H__
+#define __RTMPPUSH_H__
+
+#include <stdint.h>
+
+// version: v1.0.0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* rtmp_push_init(char *url, uint8_t *aac_dec_spec);
+void  rtmp_push_exit(void *ctxt);
+int   rtmp_push_conn(void *ctxt);
+void  rtmp_push_h264(void *ctxt, uint8_t *data, int len);
+void  rtmp_push_alaw(void *ctxt, uint8_t *data, int len);
+void  rtmp_push_aac (void *ctxt, uint8_t *data, int len);
+void  rtmp_push_url (void *ctxt, char *url);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
+
